2864f380d49d7ce0cb8671bdd92055a7f30c77b0
Sarang Wadode <sarangwadode@gmail.com>
2022-02-07 10:54:40 +0530

    sidebar component added and styled

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css'
+
+export default function Sidebar() {
+  return <>
+  <div className="logo">
+      <span className="dev">Dev</span>
+      <span className="challenges">challenges.io</span>
+  </div>
+  <div className="pages">
+      <ul>
+          <li className="page">Colors</li>
+          <li className="page">Typography</li>
+          <li className="page">Spaces</li>
+          <li className="page">Buttons</li>
+          <li className="page">Input</li>
+          <li className="page">Grid</li>
+      </ul>
+  </div>
+  </>;
+}
