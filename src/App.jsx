89fa54defadf7a1e240861fc14f0876fdaf59457
Sarang Wadode <sarangwadode@gmail.com>
2022-02-09 17:43:32 +0530

    pages arranged

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
-import  Title from './components/Title/index.jsx';
+import Sidebar from './components/Sidebar';
+import Buttons from './Pages/Buttons';
 
 function App() {
   return (
     <div className="App">
-      < Title />
+      <div className="sidebar">
+        < Sidebar />
+      </div>
+      <>
+        < Buttons />
+      </>
     </div>
   );
 }
